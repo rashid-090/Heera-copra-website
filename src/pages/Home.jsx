2a86da8bd800cdcd6022f4c7ec546075be7e1b91
Rashid <rashidpk090@gmail.com>
2025-01-14 18:04:22 +0530

    address

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import {bannerImg,heeraAbt,heeraWhy,blobshape,LogoFav} from '../assets';
 import {Faq} from '../components'
 import { MdOutlineVerified } from "react-icons/md";
 import { VscDebugBreakpointLog } from "react-icons/vsc";
-
+import Marquee from "react-fast-marquee";
 import pricing from '../assets/images/svg/Upfront pricing.svg'
 import portal from '../assets/images/svg/User friendly portal.svg'
 import experts from '../assets/images/svg/Personalized visa experts.svg'
@@ -15,9 +15,9 @@ import billingsystem from '../assets/images/svg/Unified billing system.svg'
 const Home = () => {
   return (
     <>
-    <section className='bg-home-bg bg-cover text-white h-screen overflow-hidden'>
+    <section className='bg-home-bg bg-cover text-white xl:h-screen overflow-hidden relative' >
         <div className='absolute bg-[#00000091] w-full h-full z-0'></div>
-        <div className='w-11/12 xl:w-10/12 mx-auto  h-full z-10 relative grid grid-cols-1 gap-10 gap-x-10'>
+        <div className='w-11/12 xl:w-10/12 mx-auto pt-40 py-10  h-full z-10 relative grid grid-cols-1 gap-10 gap-x-10'>
            <div className='h-full w-full flex flex-col justify-center gap-3 xl:gap-5 xl:pt-14 xl:w-[60%]'>
                 <h1 className='text-3xl xl:text-5xl font-medium capitalize'>Business Visa from<br/> Dubai made simple</h1>
                 <p className='xl:pr-32 text-base tracking-tight text-justify'>Leveraging a global network of expertise, we ensure effortless relocations worldwide for thousands of corporations by simplifying your Business visa from Dubai processes for employees both individually and as a team.</p>
@@ -27,13 +27,13 @@ const Home = () => {
         </div>
     </section>
     <section className='bg-white text-black'>
-        <div className='w-11/12 xl:w-10/12 mx-auto py-10 xl:py-20 grid grid-cols-1 md:grid-cols-2 gap-10'>
-            <div>
-                <img className='object-cover h-full w-full' loading='lazy' src={heeraAbt} alt="" />
+        <div className='w-11/12 xl:w-10/12 mx-auto py-10 xl:py-20 grid grid-cols-1 xl:grid-cols-2 gap-10'>
+            <div className='hidden md:block'>
+                <img className='object-cover h-full md:h-80 xl:h-full w-full' loading='lazy' src={heeraAbt} alt="" />
             </div>
             <div className='flex flex-col gap-3'>
-                <p className='text-mainhvr l tracking-widest'>ABOUT US</p>
-                <h2 className='text-2xl xl:text-4xl font-medium capitalize'>Making corporate global mobility simple, fast, and reliable</h2>
+                <p className='text-mainhvr  tracking-widest'>ABOUT US</p>
+                <h2 className='text-3xl xl:text-4xl font-medium capitalize'>Making corporate global mobility simple, fast, and reliable</h2>
                 <p className='text-gray-600 text-base text-justify'>At Heera Corporate, we know what it’s like to have a frequent travel team on board. Boasting an international network of companies, we ensure a flawless Dubai Business visa processing for businesses of all sizes to smoothly transition and relocate personal to their global branches.</p>
                 <p className='text-gray-600 text-base text-justify'>Our experts handle the complexities of requirements of your Business visa from Dubai, so that you can focus on what’s important in growing your business worldwide.</p>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-5 mt-3'>
@@ -63,8 +63,8 @@ const Home = () => {
         <div className='w-11/12 xl:w-10/12 mx-auto py-10 xl:py-20 grid grid-cols-1 gap-10 gap-x-10'>
             <div className='flex flex-col  gap-3 '>
                 <p className=' tracking-widest'>WHY CHOOSE US</p>
-                <h2 className='text-2xl xl:text-4xl font-medium capitalize'>Experience the Heera Corporate<br/> difference with stress-free Business visa from Dubai.</h2>
-                <div className='grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-5 mt-5'>
+                <h2 className='text-3xl xl:text-4xl font-medium capitalize'>Experience the Heera Corporate<br/> difference with stress-free Business visa from Dubai.</h2>
+                <div className='hidden md:grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 md:gap-5 mt-5'>
                     <div className='bg-white text-black p-5 rounded-2xl shadow-lg duration-150 hover:shadow-2xl flex items-center gap-3'>
                         <img className='h-10 w-10 object-cover' src={pricing} alt="" /> 
                         <div>
@@ -109,16 +109,70 @@ const Home = () => {
                     </div>
                    
                 </div>
+                <div className='flex flex-col gap-5 md:hidden'>
+                    <Marquee>
+                    <div className='bg-white text-black p-5 rounded-2xl shadow-lg duration-150 hover:shadow-2xl flex items-center gap-3 mr-5'>
+                        <img className='h-10 w-10 object-cover' src={pricing} alt="" /> 
+                        <div>
+                            <p className='text-sm xl:text-lg font-medium '>Upfront pricing</p>
+                            <p className='text-xs md:text-sm'> Flat rate transparency with no hidden charges.</p>
+                        </div>
+                    </div>
+                    <div className='bg-white text-black p-5 rounded-2xl shadow-lg duration-150 hover:shadow-2xl flex items-center gap-3 mr-5'>
+                        <img className='h-10 w-10 object-cover' src={portal} alt="" />
+                        <div>
+                            <p className='text-sm xl:text-lg font-medium '> User friendly portal</p>
+                            <p className='text-xs md:text-sm'> Simple online account management of your Dubai Business visa applications.</p>
+                        </div>
+                    </div>
+                   
+                    </Marquee>
+                    <Marquee direction='right'>
+                 
+                    <div className='bg-white text-black p-5 rounded-2xl shadow-lg duration-150 hover:shadow-2xl flex items-center gap-3 mr-5'>
+                        <img className='h-10 w-10 object-cover' src={experts} alt="" />
+                        <div>
+                            <p className='text-sm xl:text-lg font-medium '> Personalized visa experts</p>
+                            <p className='text-xs md:text-sm'> Dedicated end-to-end support with real-time status updates.</p>
+                        </div>
+                    </div>
+                    <div className='bg-white text-black p-5 rounded-2xl shadow-lg duration-150 hover:shadow-2xl flex items-center gap-3 mr-5'>
+                        <img className='h-10 w-10 object-cover' src={Prompt} alt="" />
+                       <div>
+                            <p className='text-sm xl:text-lg font-medium '> Prompt processing</p>
+                            <p className='text-xs md:text-sm'> Reliable delivery and easy Dubai Business visa processing time.</p>
+                       </div>
+                    </div>
+                    
+                    </Marquee>
+                    <Marquee>
+                   
+                    <div className='bg-white text-black p-5 rounded-2xl shadow-lg duration-150 hover:shadow-2xl flex items-center gap-3 mr-5'>
+                        <img className='h-10 w-10 object-cover' src={visaexpertise} alt="" />
+                       <div>
+                             <p className='text-sm xl:text-lg font-medium '> Proven visa expertise</p>
+                            <p className=''text-xs md:text-sm> Navigating complex visa processes with ease.</p>
+                       </div>
+                    </div>
+                    <div className='bg-white text-black p-5 rounded-2xl shadow-lg duration-150 hover:shadow-2xl flex items-center gap-3 mr-5'>
+                        <img className='h-10 w-10 object-cover' src={billingsystem} alt="" />
+                        <div>
+                            <p className='text-sm xl:text-lg font-medium '> Unified billing system</p>
+                            <p className='text-xs md:text-sm'> Simplified and accessible payments from Business visa services in Dubai.</p>
+                        </div>
+                    </div>
+                    </Marquee>
+                </div>
 
             </div>
           
         </div>
     </section>
     <section className=''>
-        <div className='w-11/12 xl:w-10/12 mx-auto py-10 xl:py-20 grid grid-cols-1 md:grid-cols-2 gap-5 gap-x-20'>
+        <div className='w-11/12 xl:w-10/12 mx-auto py-10 xl:py-20 grid grid-cols-1 xl:grid-cols-2 gap-5 gap-x-20'>
            <div className='h-full flex flex-col gap-3 justify-center'>
-                <h2 className='text-2xl xl:text-4xl font-medium capitalize'>Frequently Asked<br/> Questions</h2>
-                <p className='text-gray-600 text-base'>Answering popular queries to resolve and give you a peace of mind regarding Business visa services in Dubai.</p>
+                <h2 className='text-3xl xl:text-4xl font-medium capitalize'>Frequently Asked<br/> Questions</h2>
+                <p className='text-gray-600 text-sm md:text-base'>Answering popular queries to resolve and give you a peace of mind regarding Business visa services in Dubai.</p>
            </div>
             <div className=''>
                 <Faq/>
@@ -129,14 +183,14 @@ const Home = () => {
         {/* <img className='absolute -translate-y-[50%] top-[50%] right-0 z-0 h-80 opacity-25' src={LogoFav} alt="" /> */}
         <img className='absolute -translate-y-[50%] top-[50%] left-0 z-0 h-80 opacity-20' src={LogoFav} alt="" />
         <div className='w-11/12 z-10 relative xl:w-10/12 mx-auto py-10 xl:py-20 flex flex-col gap-3 justify-center items-center text-center'>
-                <h2 className='text-2xl xl:text-7xl font-medium capitalize'>Experience the Heera Corporate Difference.</h2>
+                <h2 className='text-4xl xl:text-7xl font-medium capitalize'>Experience the Heera Corporate Difference.</h2>
                 <p className='xl:w-[60%]'>Ready to simplify your global mobility? Contact Heera Corporate today for a free consultation. Our team of experts is available to answer your questions and guide you through the process of obtaining a Business Visa from Dubai.</p>
                 <button className='mt-5 border border-white  hover:bg-white  duration-150 w-fit hover:text-mainhvr px-5 rounded-xl py-2  font-medium capitalize'>apply visa now</button>
 
         </div>
     </section>
     <section>
-        <div className='w-11/12 xl:w-10/12 mx-auto py-10 xl:py-20 flex flex-col-reverse md:flex-row gap-10 gap-x-10'>
+        <div className='w-11/12 xl:w-10/12 mx-auto py-10 xl:py-20 flex flex-col-reverse xl:flex-row gap-10 gap-x-10'>
             <div className='basis-1/2'>
                 <form className='flex flex-col gap-3 text-sm'>
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
@@ -153,7 +207,7 @@ const Home = () => {
             </div>
             <div className='basis-1/2  flex flex-col gap-3 justify-center'>
                 <h2 className='text-3xl xl:text-4xl font-medium capitalize'>get your free<br/> consultation now</h2>
-                <p className='text-gray-600 text-base'>Fill out the form below, and our team will promptly<br/> get in touch with you.</p>
+                <p className='text-gray-600 text-sm md:text-base'>Fill out the form below, and our team will promptly<br/> get in touch with you.</p>
             </div>
         </div>
     </section>
